c777f8df9d8d5373d7cd9339324e350d04db7f82
putlx <putlx@outlook.com>
2020-10-06 02:51:31 +0800

    Mix class TableDependencies into class Converter

--- src/Converter.java
+++ src/Converter.java
@@ -5,44 +5,41 @@ import org.apache.poi.ss.usermodel.Workbook;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Base64;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.StringJoiner;
 
-class Converter {
-    private Workbook wb = null;
-    private boolean empty = false;
-    private HashMap<String, HashMap<String, TableDependencies.Column>> dependencies;
+public class Converter {
+    private DatabaseMetaData dbmd;
+    private Workbook wb;
+    private boolean empty;
+    private HashSet<String> tableNames;
+    private StringJoiner columnNames;
+    private StringJoiner conditions;
 
     public void read(String input, String[] pTableNames) throws SQLException {
         try (var con = DriverManager.getConnection("jdbc:sqlite:" + input)) {
+            dbmd = con.getMetaData();
             wb = new HSSFWorkbook();
             var sheetNum = 0;
-            var dbmd = con.getMetaData();
             var included = new HashSet<String>();
             for (var tableName : pTableNames) {
-                dependencies = new TableDependencies(dbmd).get(tableName);
-                included.addAll(dependencies.keySet());
-                var columnNames = new StringJoiner(", ");
-                for (var table : dependencies.entrySet()) {
-                    for (var column : table.getValue().values()) {
-                        if (column.imported == null) {
-                            columnNames.add(column.tableName + "." + column.name);
-                        }
-                    }
-                }
-                var pktables = new HashSet<String>();
-                var conditions = new StringJoiner(" AND ");
-                makeSQL(tableName, pktables, conditions);
+                tableNames = new HashSet<>();
+                columnNames = new StringJoiner(", ");
+                conditions = new StringJoiner(" AND ");
+                makeSQL(tableName);
+                included.addAll(tableNames);
+                tableNames.remove(tableName);
                 var sql = "SELECT " + columnNames + " FROM " + tableName;
-                if (!pktables.isEmpty()) {
-                    sql += " INNER JOIN " + String.join(", ", pktables) + " ON " + conditions;
+                if (!tableNames.isEmpty()) {
+                    sql += " INNER JOIN " + String.join(", ", tableNames) + " ON " + conditions;
                 }
                 var sheet = wb.createSheet();
-                wb.setSheetName(sheetNum, String.join("&", dependencies.keySet()));
+                tableNames.add(tableName);
+                wb.setSheetName(sheetNum, String.join("&", tableNames));
                 sheetNum++;
                 makeXLS(sheet, con, sql, true);
             }
@@ -74,13 +71,31 @@ class Converter {
         }
     }
 
-    private void makeSQL(String tableName, HashSet<String> pktables, StringJoiner conditions) {
-        for (var column : dependencies.get(tableName).values()) {
-            if (column.imported != null) {
-                pktables.add(column.imported.tableName);
-                conditions.add(tableName + "." + column.name + " = " + column.imported.tableName + "." + column.imported.name);
-                makeSQL(column.imported.tableName, pktables, conditions);
+    private void makeSQL(String tableName) throws SQLException {
+        if (tableNames.contains(tableName)) {
+            return;
+        }
+        tableNames.add(tableName);
+        var fkcolumnNames = new HashSet<String>();
+        var pktableNames = new HashSet<String>();
+        var rs = dbmd.getImportedKeys(null, null, tableName);
+        while (rs.next()) {
+            var pktableName = rs.getString("PKTABLE_NAME");
+            var pkcolumnName = rs.getString("PKCOLUMN_NAME");
+            var fkcolumnName = rs.getString("FKCOLUMN_NAME");
+            pktableNames.add(pktableName);
+            fkcolumnNames.add(fkcolumnName);
+            conditions.add(tableName + "." + fkcolumnName + " = " + pktableName + "." + pkcolumnName);
+        }
+        rs = dbmd.getColumns(null, null, tableName, null);
+        while (rs.next()) {
+            var columnName = rs.getString("COLUMN_NAME");
+            if (!fkcolumnNames.contains(columnName)) {
+                columnNames.add(tableName + "." + columnName);
             }
+        }
+        for (var pktableName : pktableNames) {
+            makeSQL(pktableName);
         }
     }
 
